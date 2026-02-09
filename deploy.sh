732741683c0420046c594697d89774f4158ecd82
Nan Li <840007853@qq.com>
2026-02-09 16:00:51 +0800

    增加自动启动

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,10 @@ fi
 # 停止并删除旧容器（如果存在）
 podman rm -f frpc 2>/dev/null
 
-# 启动frpc容器（后台运行，网桥模式）
+# 启动frpc容器（后台运行，网桥模式，开机自启动）
 podman run -d \
   --name frpc \
+  --restart=always \
   --net=bridge \
   -v "$PWD/$CONFIG_FILE:/etc/frpc.toml:ro,Z" \
   frpc:latest -c /etc/frpc.toml "$@"
